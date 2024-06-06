ad801e02af48ce73f1c8ce096bd7d32062a6a150
Daniel González Briseño <qbixmex@gmail.com>
2024-06-06 15:49:08 -0700

    React Query Devtools

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { router } from './router';
-
-//! Remover enable css source maps
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={ router } />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={ router } />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
