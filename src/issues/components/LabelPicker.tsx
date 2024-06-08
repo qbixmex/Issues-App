6755ef1622d5aae25799240f35b75fc09f9320e5
Daniel González Briseño <qbixmex@gmail.com>
2024-06-07 17:29:55 -0700

    Query Issues

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { Label } from "../../interfaces";
+
+const fetchLabels = async (): Promise<Label[]> => {
+  const response = await fetch('https://api.github.com/repos/facebook/react/labels');
+  const data: Label[] = await response.json();
+  return data;
+};
 
 export const LabelPicker = () => {
+
+  const labelQuery = useQuery({
+    queryKey: ['labels'],
+    queryFn: fetchLabels,
+  });
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
+      <span
+        className="badge rounded-pill m-1 label-picker"
+        style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+      >
+        Primary
+      </span>
     </div>
   )
 }
