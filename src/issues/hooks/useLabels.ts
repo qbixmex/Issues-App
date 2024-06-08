646c3c3c976c136c528bbc14edfb50cdcf27fe96
Daniel González Briseño <qbixmex@gmail.com>
2024-06-08 03:10:18 -0700

    Place Holder and Initial Data

--- src/issues/hooks/useLabels.ts
+++ src/issues/hooks/useLabels.ts
@@ -15,6 +15,25 @@ export const useLabels = () => {
     queryFn: fetchLabels,
     refetchOnWindowFocus: true,
     staleTime: 1000 * 60 * 60, // 1 hour
+    // initialData: [],
+    placeholderData: [
+      {
+        id: 52079258,
+        node_id: "MDU6TGFiZWw1MjA3OTI1OA==",
+        url: "https://api.github.com/repos/facebook/react/labels/Difficulty:%20starter",
+        name: "Difficulty: starter",
+        color: "94ce52",
+        default: false,
+      },
+      {
+        id: 6344006318,
+        node_id: "LA_kwDOAJy2Ks8AAAABeiHarg",
+        url: "https://api.github.com/repos/facebook/react/labels/fb-exported",
+        name: "fb-exported",
+        color: "ededed",
+        default: false,
+      }
+    ]
   });
 
 
