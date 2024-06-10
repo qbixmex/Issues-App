c8fc759dedbe4407c053209c1fd2651716b391d0
Daniel González Briseño <qbixmex@gmail.com>
2024-06-09 18:16:18 -0700

    Github Token Added

--- src/api/githubAPI.ts
+++ src/api/githubAPI.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
+const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
 
 export const githubAPI = axios.create({
   baseURL: API_URL,
-  headers: {},
+  headers: {
+    Authorization: `Bearer ${GITHUB_TOKEN}`,
+  },
 });
